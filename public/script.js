3627aece72b37a9a3e7b4c80d65a7c7e1c189c7a
s-uhith2006 <69343094+s-uhith2006@users.noreply.github.com>
2022-12-18 10:00:22 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,26 @@
+const socket = io("/");
+const user=prompt("enter your name")
+$(function () {
+    $("#send").click(function () {
+        if ($("#chat_message").val().length !== 0) {
+            console.log(user);
+            socket.emit("message", $("#chat_message").val(),user);
+            $("#chat_message").val("");
+        }
+    })
+    $("#chat_message").keydown(function (e) {
+        if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val(),user);
+            $("#chat_message").val("");
+        }
+    })
+})
+
+socket.on("createMessage", (message,userName) => {
+    $(".messages").append(`
+        <div class="message">
+        <b><span class="userName">${userName}:</span></b>
+            <span>${message}</span>
+        </div>
+    `)
+});
